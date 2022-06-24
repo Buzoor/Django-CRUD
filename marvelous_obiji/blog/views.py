828076b149b4a1610f10e16cf9a4de880ea4f2cb
Marvelous Obiji <marvelous@gmail.com>
2022-06-24 12:48:38 +0100

    django crud

---
+++ marvelous_obiji/blog/views.py
@@ -0,0 +1,27 @@
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+from .models import Post
+
+class PostListView(ListView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = Post
+    success_url = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = Post
+    success_url = reverse_lazy("blog:all")
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = Post
+    success_url = reverse_lazy("blog:all")
